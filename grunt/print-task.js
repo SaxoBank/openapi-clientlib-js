8c490b1932c245af1b08ca51525b90c4423052b0
Luke Page <luke.a.page@gmail.com>
2017-03-10 13:31:43 +0100

    Remove jsdoc build and link to doclets.io clearer
    Fixes #16

--- grunt/print-task.js
+++ grunt/print-task.js
@@ -10,9 +10,6 @@ var txt = {
 	'coverage': [
 		bold + 'Generated code coverage report has been placed in dist/coverage' + reset
 	],
-	'jsdoc': [
-		bold + 'Generated documentation has been placed in dist/doc' + reset
-	],
 	'release': [
 		bold + 'Generated js library has been placed in ' + sharedConfig.release + reset
 	],
@@ -30,9 +27,7 @@ var txt = {
 	'helptext': [
 		bold + 'The following grunt commands are available' + reset,
 		'',
-		bold + indent + 'grunt jsdoc' + reset + ' (Build the documentation of the library)',
 		bold + indent + 'grunt dist' + reset + ' (Bundle the source into a single distributable file)',
-		bold + indent + 'grunt publish' + reset + ' (Will build both release and docs, test the release and drop the generated files into a dropfolder)',
 		bold + indent + 'grunt test' + reset + ' (Run our unittests and build a code coverage report)',
 		bold + indent + 'grunt test-watch' + reset + ' (Run the unittests under a watch - auto reruns tests when code changes)',
 		bold + indent + 'grunt test-server' + reset + ' (Run the unittests in your own browser - sets up a server you can connect to)',
