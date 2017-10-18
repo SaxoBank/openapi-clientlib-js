09efe6b787f2e21f1d878c74a784d4e9c1a68c1f
James Walker <j.a.walker88@gmail.com>
2017-10-18 12:53:19 +0100

    Add functionality to unsubscribe by tag

--- src/openapi/streaming/subscription-actions.js
+++ src/openapi/streaming/subscription-actions.js
@@ -2,6 +2,7 @@ export const ACTION_SUBSCRIBE = 0x1;
 export const ACTION_UNSUBSCRIBE = 0x2;
 export const ACTION_MODIFY = 0x4;
 export const ACTION_MODIFY_PATCH = 0x8;
+export const ACTION_UNSUBSCRIBE_BY_TAG_PENDING = 0x10;
 
 // Subscrtiption action variant that drives queue merging logic for subscription modification.
 // Whenever ACTION_UNSUBSCRIBE is followed by ACTION_MODIFY_SUBSCRIBE, both actions will be kept.
