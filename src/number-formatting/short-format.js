4650ab4bb7cf25375cf5f08cc31d5c55b70c3b53
varnikaj <varnikaj@gmail.com>
2018-07-12 07:05:50 +0000

    refactor short-format

--- src/number-formatting/short-format.js
+++ src/number-formatting/short-format.js
@@ -24,23 +24,25 @@ function shortFormat(num, options) {
     let prefix = '';
     let suffix = '';
 
-    const notationMap = {
-        BILLION: {
-            shortNotation: 'bn',
-            digitSize: 10,
-            precisionDigits: 10,
-        },
-        MILLION: {
-            shortNotation: 'm',
-            digitSize: 7,
-            precisionDigits: 7,
-        },
-        THOUSAND: {
-            shortNotation: 'k',
-            digitSize: 5,
-            precisionDigits: 4,
-        },
-    };
+    // denominations should always be in sorted order
+    const notations = [{
+        shortNotation: 'bn',
+        digitSize: 10,
+        precisionDigits: 10,
+        numDigits: 9,
+    },
+    {
+        shortNotation: 'm',
+        digitSize: 7,
+        precisionDigits: 7,
+        numDigits: 6,
+    },
+    {
+        shortNotation: 'k',
+        digitSize: 5,
+        precisionDigits: 4,
+        numDigits: 3,
+    }];
 
     if (num < 0) { // -10000 => -10k
         num = Math.abs(num);
@@ -51,31 +53,28 @@ function shortFormat(num, options) {
     const shortFormatOptions = extend({}, options, { isHideZeroTail: true });
     const [digits] = String(num).split('.');
 
-    let notation;
     let numberPrecision = 0
-    let shortNotation = '';
+    let shortHandNotation = '';
 
     let digitSize = roundOffNumber(num, digits.length);
 
-    if (digitSize >= notationMap['BILLION'].digitSize) { // >= 1,000,000,000
-        notation = 'BILLION';
-        numberPrecision = (2 - (digitSize - precisionDigits));
-        num = num / Math.pow(10, 9);
-    } else if(digitSize >= notationMap['MILLION'].digitSize) { // >= 1,000,000
-        notation = 'MILLION';
-        num = num / Math.pow(10, 6);
-    } else if(digitSize >= notationMap['THOUSAND'].digitSize) { // >= 10,000
-        notation = 'THOUSAND';
-        num = num / Math.pow(10, 3);
+    for(let i = 0; i < notations.length; i++) {
+        const notation = notations[i];
+        if(digitSize >= notation.digitSize) {
+            const {
+                precisionDigits,
+                numDigits,
+                shortNotation
+            } = notation;
+
+            numberPrecision = (2 - (digitSize - precisionDigits));
+            num = num / Math.pow(10, numDigits);
+            shortHandNotation = shortNotation;
+            break;
+        }
     }
 
-    if(notationMap[notation]) {
-        const precisionDigits = notationMap[notation].precisionDigits
-        shortNotation = notationMap[notation].shortNotation;
-        numberPrecision = (2 - (digitSize - precisionDigits));
-    }
-
-    return `${prefix}${formatNumber(num, numberPrecision, shortFormatOptions)}${suffix}${shortNotation}`;
+    return `${prefix}${formatNumber(num, numberPrecision, shortFormatOptions)}${suffix}${shortHandNotation}`;
 }
 
 function roundOffNumber(num, digitSize) {
@@ -84,7 +83,6 @@ function roundOffNumber(num, digitSize) {
 
     // If number is greater than 10,000 round off to closest number and increase digitSize accordingly. 
     // Eg   99,950 is closer to 100,000 than 99.9k, so new digitSize will be increased to 6
-
     if (digitSize >= 5) { // bigger than 10,000
         boundary = Math.pow(10, digitSize) - (Math.pow(10, digitSize - 3) / 2); // e.g.  99,950 - closer to 100k than 99.9k
         if (num >= boundary) {
