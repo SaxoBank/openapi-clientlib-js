c494d7e2ce2adf02b11888acef55b9b46cee31ed
Michal Kulinski <xmiku@saxobank.com>
2020-06-12 11:14:12 +0200

    Improve logging around streaming (#184)
    * Improved streaming logging.

--- src/openapi/streaming/connection/transport/websocket-transport.js
+++ src/openapi/streaming/connection/transport/websocket-transport.js
@@ -190,7 +190,7 @@ function handleSocketMessage(messageEvent) {
             return;
         }
 
-        log.debug('Parsed message', {
+        log.debug(LOG_AREA, 'Parsed message', {
             messages: parsedMessages,
         });
 
