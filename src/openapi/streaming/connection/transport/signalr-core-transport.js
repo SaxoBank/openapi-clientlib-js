0efd0207bd6b3b92163c770d7a2210ef9e061e02
yugandhar02 <yugandhar.kamdi@gmail.com>
2020-09-25 19:39:27 +0530

    Add signalr core support

---
+++ src/openapi/streaming/connection/transport/signalr-core-transport.js
@@ -0,0 +1,283 @@
+import fetch from '../../../../utils/fetch';
+import log from '../../../../log';
+import * as constants from './../constants';
+
+const NAME = 'signalrCore';
+const LOG_AREA = 'SignalrCoreTransport';
+const NOOP = () => {};
+
+/**
+ * Handles any signal-r log, and pipes it through our logging.
+ * @param message
+ */
+function handleLog(level, message) {
+    if (level < window.signalrCore.LogLevel.Error) {
+        return;
+    }
+
+    log.error(LOG_AREA, message);
+}
+
+function buildConnection({ baseUrl, contextId, authToken, protocol }) {
+    const accessTokenFactory = () => {
+        return authToken.replace('BEARER ', '');
+    };
+    const url = `${baseUrl}/oapi/streaming?contextId=${contextId}`;
+
+    return new window.signalrCore.HubConnectionBuilder()
+        .withUrl(url, {
+            accessTokenFactory,
+        })
+        .withHubProtocol(protocol)
+        .configureLogging({
+            log: handleLog,
+        })
+        .build();
+}
+
+function parseMessage(message, utf8Decoder) {
+    let dataFormat;
+    let data;
+
+    const { referenceId, payloadFormat, payload } = message;
+
+    // JSON
+    if (payloadFormat === 1) {
+        try {
+            // use old streaming service's data format for backward compatibility
+            dataFormat = 0;
+
+            data = utf8Decoder.decode(new Uint8Array(payload));
+            data = JSON.parse(data);
+        } catch (e) {
+            const error = new Error(e.message);
+            error.payload = data;
+
+            throw error;
+        }
+    } else {
+        // Protobuf
+        dataFormat = 1;
+        data = new Uint8Array(payload);
+    }
+
+    return {
+        ReferenceId: referenceId,
+        DataFormat: dataFormat,
+        Data: data,
+    };
+}
+
+function SignalrCoreTransport(baseUrl, transportFailCallback = NOOP) {
+    this.name = NAME;
+    this.baseUrl = baseUrl;
+    this.connection = null;
+    this.authToken = null;
+    this.contextId = null;
+    this.messageStream = null;
+    this.hasStreamingStarted = false;
+
+    // callbacks
+    this.transportFailCallback = transportFailCallback;
+    this.stateChangedCallback = NOOP;
+    this.receivedCallback = NOOP;
+    this.errorCallback = NOOP;
+    this.unauthorizedCallback = NOOP;
+
+    try {
+        this.utf8Decoder = new window.TextDecoder();
+    } catch (error) {
+        log.error(LOG_AREA, 'Error occured while initializing text decoder', {
+            error,
+        });
+
+        transportFailCallback(error);
+    }
+}
+
+SignalrCoreTransport.prototype.isSupported = function() {
+    return Boolean(window.Uint8Array) && Boolean(window.TextDecoder);
+};
+
+SignalrCoreTransport.prototype.start = function(options, onStartCallback) {
+    if (this.connection) {
+        log.warn(
+            LOG_AREA,
+            'connection already exist, close the exisiting conection before starting new one',
+        );
+        return;
+    }
+
+    try {
+        this.connection = buildConnection({
+            baseUrl: this.baseUrl,
+            contextId: this.contextId,
+            authToken: this.authToken,
+            protocol: options.protocol,
+        });
+    } catch (error) {
+        this.transportFailCallback(error);
+    }
+
+    this.connection.onclose = this.handleConnectionClosure.bind(this);
+
+    this.stateChangedCallback(constants.CONNECTION_STATE_CONNECTING);
+
+    return this.connection
+        .start()
+        .then(() => {
+            const messageStream = this.connection.stream('StartStreaming');
+
+            messageStream.subscribe({
+                next: this.handleNextMessage.bind(this),
+                error: this.handleMessageStreamError.bind(this),
+            });
+
+            if (onStartCallback) {
+                onStartCallback();
+            }
+
+            this.stateChangedCallback(constants.CONNECTION_STATE_CONNECTED);
+
+            this.messageStream = messageStream;
+        })
+        .catch((error) => {
+            this.transportFailCallback(error);
+        });
+};
+
+SignalrCoreTransport.prototype.stop = function() {
+    if (!this.connection) {
+        log.warn(LOG_AREA, "connection doesn't exist");
+        return;
+    }
+
+    // close message stream before closing connection
+    return this.messageStream
+        .cancelCallback()
+        .then(() => this.connection.stop());
+};
+
+SignalrCoreTransport.prototype.handleConnectionClosure = function(error) {
+    if (error) {
+        log.error(LOG_AREA, 'connection closed abruptly', { error });
+    }
+
+    this.connection = null;
+
+    if (!this.hasStreamingStarted) {
+        this.transportFailCallback(error);
+        return;
+    }
+
+    this.hasStreamingStarted = false;
+    this.stateChangedCallback(constants.CONNECTION_STATE_DISCONNECTED);
+};
+
+SignalrCoreTransport.prototype.handleNextMessage = function(message) {
+    if (!this.hasStreamingStarted) {
+        this.hasStreamingStarted = true;
+    }
+
+    const parsedMessage = parseMessage(message, this.utf8Decoder);
+
+    this.receivedCallback(parsedMessage);
+};
+
+SignalrCoreTransport.prototype.handleMessageStreamError = function(error) {
+    // It will be called if signalr failed to send message to start streaming
+    // or if connection is closed with some error
+    // only handle the 1st case since connection closing with error is already handled in onclose handler
+    if (!this.hasStreamingStarted) {
+        log.error(LOG_AREA, 'Error occured while starting message streaming', {
+            error,
+        });
+
+        this.connection.stop();
+    }
+};
+
+SignalrCoreTransport.prototype.updateQuery = function(
+    authToken,
+    contextId,
+    forceAuth = false,
+) {
+    log.debug(LOG_AREA, 'Updated query', {
+        contextId,
+        forceAuth,
+    });
+
+    this.contextId = contextId;
+    this.authToken = authToken;
+
+    if (forceAuth) {
+        this.renewSession(authToken, contextId);
+    }
+};
+
+SignalrCoreTransport.prototype.renewSession = function(authToken, contextId) {
+    const options = {
+        headers: {
+            Authorization: authToken,
+        },
+    };
+    const url = `${this.baseUrl}/oapi/streaming/renewal/renewsession`;
+
+    return fetch('POST', url, options)
+        .then(() => {
+            log.debug(
+                LOG_AREA,
+                'Streaming session is successfully renewed with new token',
+                {
+                    contextId,
+                },
+            );
+        })
+        .catch((error) => {
+            // if this call was superceded by another one, then ignore this error
+            if (this.authToken !== authToken) {
+                return;
+            }
+
+            // if a network error occurs, retry
+            if (error && error.isNetworkError) {
+                return this.renewSession(authToken, contextId);
+            }
+
+            if (error.status === 401) {
+                this.unauthorizedCallback();
+                return;
+            }
+
+            log.error(
+                LOG_AREA,
+                'Error occured during streaming session renewal',
+                {
+                    contextId,
+                    error,
+                },
+            );
+
+            this.transportFailCallback(error);
+        });
+};
+
+SignalrCoreTransport.prototype.setStateChangedCallback = function(callback) {
+    this.stateChangedCallback = callback;
+};
+
+SignalrCoreTransport.prototype.setReceivedCallback = function(callback) {
+    this.receivedCallback = callback;
+};
+
+SignalrCoreTransport.prototype.setErrorCallback = function(callback) {
+    this.errorCallback = callback;
+};
+
+SignalrCoreTransport.prototype.setUnauthorizedCallback = function(callback) {
+    this.unauthorizedCallback = callback;
+};
+
+SignalrCoreTransport.prototype.setConnectionSlowCallback = NOOP;
+
+export default SignalrCoreTransport;
---
+++ src/openapi/streaming/connection/transport/signalr-core-transport.spec.js
@@ -0,0 +1,314 @@
+import { installClock, uninstallClock, tick } from '../../../../test/utils';
+import '../../../../test/mocks/math-random';
+import mockFetch from '../../../../test/mocks/fetch';
+import * as constants from './../constants';
+import jsonPayload from './payload.json';
+import SignalrCoreTransport from './signalr-core-transport';
+
+const CONTEXT_ID = '0000000000';
+const AUTH_TOKEN = 'TOKEN';
+const BASE_URL = 'testUrl';
+
+const NOOP = () => {};
+
+describe('openapi SignalR Transport', () => {
+    let subscribeNextHandler = NOOP;
+    let subscribeErrorHandler = NOOP;
+    let startPromiseResolver = NOOP;
+    let startPromiseRejector = NOOP;
+    let streamCancelPromiseResolver = NOOP;
+
+    let mockHubConnection;
+    let spyOnMessageStream;
+    let spyOnConnectionStop;
+    let spyOnStartCallback;
+    let spyOnStateChangedCallback;
+    let spyOnTransportFailedCallback;
+    let fetchMock;
+
+    class MockConnectionBuilder {
+        withUrl() {
+            return this;
+        }
+
+        withHubProtocol() {
+            return this;
+        }
+
+        configureLogging() {
+            return this;
+        }
+
+        build() {
+            return mockHubConnection;
+        }
+    }
+
+    window.signalrCore = {
+        HubConnectionBuilder: MockConnectionBuilder,
+    };
+
+    beforeEach(() => {
+        const mockSubject = {
+            subscribe: ({ next, error }) => {
+                subscribeNextHandler = next;
+                subscribeErrorHandler = error;
+            },
+            cancelCallback: () =>
+                new Promise((resolve) => {
+                    streamCancelPromiseResolver = resolve;
+                }),
+        };
+
+        spyOnMessageStream = jest.fn().mockImplementation(() => mockSubject);
+        spyOnConnectionStop = jest
+            .fn()
+            .mockImplementation(() =>
+                setTimeout(() => mockHubConnection.onclose()),
+            );
+
+        mockHubConnection = {
+            start: () =>
+                new Promise((resolve, reject) => {
+                    startPromiseResolver = resolve;
+                    startPromiseRejector = reject;
+                }),
+            stream: spyOnMessageStream,
+            stop: spyOnConnectionStop,
+        };
+
+        fetchMock = mockFetch();
+        installClock();
+
+        spyOnStartCallback = jest.fn().mockName('spyStartCallback');
+        spyOnStateChangedCallback = jest
+            .fn()
+            .mockName('spyStateChangedCallback');
+        spyOnTransportFailedCallback = jest.fn().mockName('transportFailed');
+    });
+
+    afterEach(() => {
+        uninstallClock();
+        subscribeNextHandler = NOOP;
+        subscribeErrorHandler = NOOP;
+        startPromiseResolver = NOOP;
+        startPromiseRejector = NOOP;
+        streamCancelPromiseResolver = NOOP;
+    });
+
+    describe('start', () => {
+        let startPromise;
+
+        beforeEach(() => {
+            const transport = new SignalrCoreTransport(
+                BASE_URL,
+                spyOnTransportFailedCallback,
+            );
+            transport.setStateChangedCallback(spyOnStateChangedCallback);
+
+            transport.updateQuery(AUTH_TOKEN, CONTEXT_ID);
+            startPromise = transport.start({}, spyOnStartCallback);
+        });
+
+        it('should create message stream and call state change handler upon transport start invocation', (done) => {
+            expect(spyOnStartCallback).not.toBeCalled();
+            expect(spyOnStateChangedCallback).toHaveBeenNthCalledWith(
+                1,
+                constants.CONNECTION_STATE_CONNECTING,
+            );
+
+            // resolve handshake request
+            startPromiseResolver();
+
+            startPromise.then(() => {
+                expect(spyOnStateChangedCallback).toHaveBeenNthCalledWith(
+                    2,
+                    constants.CONNECTION_STATE_CONNECTED,
+                );
+                expect(spyOnStartCallback).toBeCalledTimes(1);
+                expect(spyOnMessageStream).toBeCalledTimes(1);
+
+                done();
+            });
+        });
+
+        it('should call transport fail callback if handshake request fails', (done) => {
+            expect(spyOnStateChangedCallback).toHaveBeenCalledWith(
+                constants.CONNECTION_STATE_CONNECTING,
+            );
+
+            // fail handshake request
+            startPromiseRejector();
+
+            startPromise.then(() => {
+                expect(spyOnStateChangedCallback).toBeCalledTimes(1);
+                expect(spyOnStartCallback).not.toBeCalled();
+                expect(spyOnTransportFailedCallback).toBeCalledTimes(1);
+                done();
+            });
+        });
+
+        it('should call transport fail callback if error is received while starting message streaming', (done) => {
+            // resolve handshake request
+            startPromiseResolver();
+
+            startPromise.then(() => {
+                subscribeErrorHandler(new Error('Streaming error'));
+                tick(10);
+
+                expect(spyOnTransportFailedCallback).toBeCalledTimes(1);
+
+                // should not change state to disconnected
+                expect(spyOnStateChangedCallback).toBeCalledTimes(2);
+                done();
+            });
+        });
+    });
+
+    describe('on message received', () => {
+        it('should parse message correctly', () => {
+            const spyReceivedCallback = jest
+                .fn()
+                .mockName('spyReceivedCallback');
+            const transport = new SignalrCoreTransport(
+                BASE_URL,
+                spyOnTransportFailedCallback,
+            );
+            transport.setReceivedCallback(spyReceivedCallback);
+
+            transport.handleNextMessage({
+                referenceId: '12',
+                payloadFormat: 1,
+                payload: new window.TextEncoder().encode(
+                    JSON.stringify(jsonPayload),
+                ),
+            });
+
+            expect(spyReceivedCallback).toBeCalledWith({
+                ReferenceId: '12',
+                DataFormat: 0,
+                Data: jsonPayload,
+            });
+        });
+    });
+
+    describe('updateQuery', () => {
+        it('should renew session on token update', () => {
+            const transport = new SignalrCoreTransport(BASE_URL);
+            transport.updateQuery(AUTH_TOKEN, CONTEXT_ID, true);
+
+            expect(fetchMock).toBeCalledTimes(1);
+            expect(fetchMock).toBeCalledWith(
+                'testUrl/oapi/streaming/renewal/renewsession',
+                expect.objectContaining({
+                    headers: { Authorization: 'TOKEN' },
+                }),
+            );
+        });
+    });
+
+    describe('when renewal call fails', () => {
+        let transport;
+        let renewalPromise;
+
+        beforeEach(() => {
+            transport = new SignalrCoreTransport(
+                BASE_URL,
+                spyOnTransportFailedCallback,
+            );
+
+            // update instance variables
+            transport.updateQuery(AUTH_TOKEN, CONTEXT_ID);
+
+            renewalPromise = transport.renewSession(
+                AUTH_TOKEN,
+                CONTEXT_ID,
+                true,
+            );
+
+            expect(fetchMock).toBeCalledTimes(1);
+            expect(fetchMock).toBeCalledWith(
+                'testUrl/oapi/streaming/renewal/renewsession',
+                expect.objectContaining({
+                    headers: { Authorization: 'TOKEN' },
+                }),
+            );
+        });
+
+        it('should call transport fail callback ', (done) => {
+            fetchMock.resolve('404');
+
+            renewalPromise.then(() => {
+                expect(spyOnTransportFailedCallback).toBeCalled();
+                done();
+            });
+        });
+
+        it('should retry renewal if there is a network error', (done) => {
+            // mock before it calls renewSession again
+            transport.renewSession = jest
+                .fn()
+                .mockImplementation(() => Promise.resolve());
+
+            fetchMock.reject(new Error('Network error'));
+
+            renewalPromise.then(() => {
+                expect(transport.renewSession).toBeCalledTimes(1);
+                expect(spyOnTransportFailedCallback).not.toBeCalled();
+                done();
+            });
+        });
+
+        it('should ignore if token is updated before prev response was received', (done) => {
+            transport.updateQuery('NEW_TOKEN', CONTEXT_ID);
+            fetchMock.resolve('401');
+
+            renewalPromise.then(() => {
+                expect(spyOnTransportFailedCallback).not.toBeCalled();
+                done();
+            });
+        });
+    });
+
+    describe('stop', () => {
+        it('should close message stream before closing connection', (done) => {
+            const transport = new SignalrCoreTransport(BASE_URL);
+            transport.setStateChangedCallback(spyOnStateChangedCallback);
+            transport.setReceivedCallback(jest.fn());
+
+            transport.updateQuery(AUTH_TOKEN, CONTEXT_ID);
+            const startPromise = transport.start({});
+
+            // resolve handshake request
+            startPromiseResolver();
+
+            startPromise.then(() => {
+                expect(spyOnStateChangedCallback).toHaveBeenNthCalledWith(
+                    2,
+                    constants.CONNECTION_STATE_CONNECTED,
+                );
+
+                // start message streaming
+                subscribeNextHandler({});
+
+                const stopPromise = transport.stop();
+
+                expect(spyOnConnectionStop).not.toBeCalled();
+
+                streamCancelPromiseResolver();
+
+                stopPromise.then(() => {
+                    expect(spyOnConnectionStop).toBeCalledTimes(1);
+
+                    tick(10);
+
+                    expect(spyOnStateChangedCallback).toHaveBeenNthCalledWith(
+                        3,
+                        constants.CONNECTION_STATE_DISCONNECTED,
+                    );
+                    done();
+                });
+            });
+        });
+    });
+});
