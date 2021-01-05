0cab36f0f6ba304e3561078d9a759b0497d9d381
Bartek <bartlomiej.kozar@gmail.com>
2021-01-05 13:45:41 +0100

    Bug fix rounding decimals formatters

--- src/number-formatting/format.js
+++ src/number-formatting/format.js
@@ -11,6 +11,11 @@ function formatNegativeNumber(str, options) {
     return options.negativePattern.replace('{0}', str);
 }
 
+function isScientificNotation(number) {
+    const numberString = String(number);
+    return /\d+\.?\d*e[+-]*\d+/i.test(numberString);
+}
+
 /**
  * converts a number to a decimal string if it is on scientific notation
  * @param number
@@ -19,7 +24,7 @@ function convertNumbertToString(number, precision) {
     let numberString = String(number);
 
     // if the number is in scientific notation, convert to decimal
-    if (/\d+\.?\d*e[+-]*\d+/i.test(numberString)) {
+    if (isScientificNotation(number)) {
         numberString = number.toFixed(precision).trim('0');
     }
 
@@ -33,12 +38,7 @@ function convertNumbertToString(number, precision) {
  * @param { groupSizes, groupSeparator, decimalSeparator, isHideZeroTail } options
  */
 function expandNumber(number, precision, options) {
-    const {
-        groupSizes,
-        groupSeparator,
-        decimalSeparator,
-        isHideZeroTail,
-    } = options;
+    const { groupSizes, groupSeparator, decimalSeparator, isHideZeroTail } = options;
     let curSize = groupSizes[0];
     let curGroupIndex = 1;
     let numberString = convertNumbertToString(number, precision);
@@ -76,10 +76,7 @@ function expandNumber(number, precision, options) {
         if (curSize === 0 || curSize > stringIndex) {
             if (ret.length > 0) {
                 return (
-                    numberString.slice(0, stringIndex + 1) +
-                    groupSeparator +
-                    ret +
-                    right
+                    numberString.slice(0, stringIndex + 1) + groupSeparator + ret + right
                 );
             }
 
@@ -92,10 +89,7 @@ function expandNumber(number, precision, options) {
                 groupSeparator +
                 ret;
         } else {
-            ret = numberString.slice(
-                stringIndex - curSize + 1,
-                stringIndex + 1,
-            );
+            ret = numberString.slice(stringIndex - curSize + 1, stringIndex + 1);
         }
 
         stringIndex -= curSize;
@@ -105,9 +99,24 @@ function expandNumber(number, precision, options) {
             curGroupIndex++;
         }
     }
-    return (
-        numberString.slice(0, stringIndex + 1) + groupSeparator + ret + right
-    );
+    return numberString.slice(0, stringIndex + 1) + groupSeparator + ret + right;
+}
+
+// Does AwayFromZero rounding as per C# - see MidpointRound.AwayFromZero
+// When a number is halfway between two others, it is rounded toward the nearest number that is away from zero.
+// We do this by rounding the absolute number, so it always goes away from zero.
+function roundNumber(number, decimals) {
+    const factor = Math.pow(10, decimals);
+    const absoluteNumber = Math.abs(number);
+    const isScientificNotationNumber = isScientificNotation(absoluteNumber);
+
+    // for not exponential numbers we use exponential rounding to avoid binary floating-point issue like 1.005 * 100 !== 100.5
+    if (isScientificNotationNumber) {
+        return Math.round(absoluteNumber * factor) / factor;
+    }
+    const roundedNumber = Math.round(`${absoluteNumber}e${decimals}`);
+
+    return Number(`${roundedNumber}e-${decimals}`);
 }
 
 // -- Exported methods section --
@@ -117,18 +126,9 @@ function formatNumber(inputNumber, decimals, options) {
         return '';
     }
 
-    // Does AwayFromZero rounding as per C# - see MidpointRound.AwayFromZero
-    // When a number is halfway between two others, it is rounded toward the nearest number that is away from zero.
-    // We do this by rounding the absolute number, so it always goes away from zero.
-    const factor = Math.pow(10, decimals);
-    let absoluteNumber = Math.abs(inputNumber);
-    absoluteNumber = Math.round(absoluteNumber * factor) / factor;
+    const absoluteNumber = roundNumber(inputNumber, decimals);
 
-    let formattedNumber = expandNumber(
-        Math.abs(absoluteNumber),
-        decimals,
-        options,
-    );
+    let formattedNumber = expandNumber(Math.abs(absoluteNumber), decimals, options);
 
     // if the original is negative and it hasn't been rounded to 0
     if (inputNumber < 0 && absoluteNumber !== 0) {
