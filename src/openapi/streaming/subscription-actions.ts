6332246bcc20265968baf1e4baca518e0364a71d
Robat Williams <XROBW@saxobank.com>
2021-07-16 14:57:54 +0100

    hex progression

--- src/openapi/streaming/subscription-actions.ts
+++ src/openapi/streaming/subscription-actions.ts
@@ -3,7 +3,7 @@ export const ACTION_UNSUBSCRIBE = 0x2 as const;
 export const ACTION_MODIFY = 0x4 as const;
 export const ACTION_MODIFY_PATCH = 0x8 as const;
 export const ACTION_MODIFY_REPLACE = 0x10 as const;
-export const ACTION_UNSUBSCRIBE_BY_TAG_PENDING = 0x12 as const;
+export const ACTION_UNSUBSCRIBE_BY_TAG_PENDING = 0x20 as const;
 
 export type SubscriptionAction =
     | typeof ACTION_SUBSCRIBE
