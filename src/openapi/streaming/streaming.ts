61b8e03ccfec3cc9b2da734264d2225d8e329ca9
Luke Page <lukp@saxobank.com>
2021-10-18 21:02:02 +0100

    Fix a bug with removing during connection issues

--- src/openapi/streaming/streaming.ts
+++ src/openapi/streaming/streaming.ts
@@ -1048,7 +1048,7 @@ class Streaming extends MicroEmitter<EmittedEvents> {
                 subscription.onUnsubscribeByTagPending();
 
                 if (shouldDisposeSubscription) {
-                    this.removeSubscription(subscription);
+                    subscription.onRemove();
                 }
             }
         }).then(() => {
@@ -1103,14 +1103,6 @@ class Streaming extends MicroEmitter<EmittedEvents> {
         });
     }
 
-    private removeSubscription(subscription: Subscription) {
-        subscription.dispose();
-        const indexOfSubscription = this.subscriptions.indexOf(subscription);
-        if (indexOfSubscription >= 0) {
-            this.subscriptions.splice(indexOfSubscription, 1);
-        }
-    }
-
     private onSubscribeNetworkError() {
         this.connection.onSubscribeNetworkError();
     }
@@ -1143,19 +1135,28 @@ class Streaming extends MicroEmitter<EmittedEvents> {
             normalizedSubscriptionArgs.Format = ParserFacade.getDefaultFormat();
         }
 
-        options = {
-            onNetworkError: this.onSubscribeNetworkError.bind(this),
+        let subscription: Subscription;
+
+        const internalOptions = {
             ...options,
+            onNetworkError: this.onSubscribeNetworkError.bind(this),
+            onSubscriptionReadyToRemove: () => {
+                const indexOfSubscription =
+                    this.subscriptions.indexOf(subscription);
+                if (indexOfSubscription >= 0) {
+                    this.subscriptions.splice(indexOfSubscription, 1);
+                }
+            },
         };
 
-        const subscription = new Subscription(
+        subscription = new Subscription(
             this.contextId, // assuming contextId exists at this stage
             this.transport,
             servicePath,
             url,
             normalizedSubscriptionArgs,
             this.onSubscriptionCreated.bind(this),
-            options,
+            internalOptions,
         );
 
         this.subscriptions.push(subscription);
@@ -1218,7 +1219,7 @@ class Streaming extends MicroEmitter<EmittedEvents> {
      */
     disposeSubscription(subscription: Subscription) {
         this.unsubscribe(subscription);
-        this.removeSubscription(subscription);
+        subscription.onRemove();
     }
 
     /**
--- src/openapi/streaming/subscription-actions.ts
+++ src/openapi/streaming/subscription-actions.ts
@@ -4,6 +4,7 @@ export const ACTION_MODIFY = 0x4 as const;
 export const ACTION_MODIFY_PATCH = 0x8 as const;
 export const ACTION_MODIFY_REPLACE = 0x10 as const;
 export const ACTION_UNSUBSCRIBE_BY_TAG_PENDING = 0x20 as const;
+export const ACTION_REMOVE = 0x40 as const;
 
 export type SubscriptionAction =
     | typeof ACTION_SUBSCRIBE
@@ -11,4 +12,5 @@ export type SubscriptionAction =
     | typeof ACTION_MODIFY
     | typeof ACTION_MODIFY_PATCH
     | typeof ACTION_MODIFY_REPLACE
-    | typeof ACTION_UNSUBSCRIBE_BY_TAG_PENDING;
+    | typeof ACTION_UNSUBSCRIBE_BY_TAG_PENDING
+    | typeof ACTION_REMOVE;
